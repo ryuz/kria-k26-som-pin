6d6e04f836a5b2e423220ba1c62ee85f6afe79da
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-01-18 21:28:07 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Kria K26 SOM ピン配置作成補助用テーブル生成ツール
+
+
+## 概要
+
+Kria K26 SOM ピン配置作成補助用テーブル生成ツールです。
+
+
+KV260 などを利用する際に、回路図から Vivado の xdc に指定するピン名にたどり着くまでに
+
+- K26 SOM コネクタに名付けられた信号名
+- K26 SOM コネクタのピン番号
+- K26 SOM に搭載された XCK26-SFVC784-2LV-C/I のピン番号
+
+と追っていかなければなりません。
+
+この際、参照する資料も
+
+- KV260 の回路図などの資料
+- [Kria K26 SOM Data Sheet (DS987)](https://docs.xilinx.com/r/en-US/ds987-k26-som)
+- [Kria SOM Carrier Card Design Guide (UG1091)](https://docs.xilinx.com/r/en-US/ug1091-carrier-card-design)内からリンクしてたどり着く XTP685(xtp685-kria-k26-som-xdc.zip) を解凍して得られる Kria_K26_SOM_Rev1.xdc
+  
+と、分かれておりなかなか不便です。
+
+特に XTP685 はダウンロードに署名が必要であり、加工したものを勝手に再配布するわけにもいきません。
+
+そこで、各自で正規手順で Kria_K26_SOM_Rev1.xdc を入手して、手元に置いて実行すれば、見やすいテーブルが生成されるスクリプトを作ってみました。
+
+## 内容物
+
+- k26-som-connector-pin.csv : DS987 内のテーブルを csv に変換したもの
+- k26_som_xdc_to_csv.py : Kria_K26_SOM_Rev1.xdc を csv に変換する python スクリプト
+
+
+## 使い方
+
+pandas を使いますので、事前にインストールください。
+
+```
+pip install pandas
+```
+
+その後、入手した Kria_K26_SOM_Rev1.xdc をカレントディレクトリにコピーして
+
+```
+python k26_som_xdc_to_csv.py
+```
+
+と実行すれば、k26-som-xck26-sfvc784-pin.csv が生成されます。
+
+
+
+
+
+
+
+
+
+
---
+++ k26_som_xdc_to_csv.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import pandas as pd
+
+
+def k26_som_xdc_to_csv(xdc_fname, csv_fname):
+    # 読み込む
+    with open(xdc_fname) as f:
+        src = f.readlines()
+
+    pin_dict = {}
+
+    # 先頭2行捨てる
+    for line in src[2:]:
+        # pin
+        result = re.match('.*PACKAGE_PIN (\S+) ', line)
+        if result is None: assert(0)
+        pin = result.group(1)
+        pin_dict[pin] = {}
+
+        # SOM connector 1
+        result = re.match('set_property\s+PACKAGE_PIN\s+([A-Z]+[0-9]+)\s+\[get_ports\s+\"(.*)\".*Bank\s+([0-9]+)\s+VCCO\s+-\s(\S+)\s+-\s(\S+)', line)
+        if result:
+            pin = result.group(1)
+            pin_dict[pin]['port'] = result.group(2)
+            pin_dict[pin]['bank'] = result.group(3)
+            pin_dict[pin]['vcco'] = result.group(4)
+            pin_dict[pin]['type'] = result.group(5)
+            continue
+
+        # SOM connector 2
+        result = re.match('set_property\s+PACKAGE_PIN\s+([A-Z]+[0-9]+)\s+\[get_ports\s+\"(.*)\".*Bank\s+([0-9]+)\s+-\s(\S+)', line)
+        if result:
+            pin = result.group(1)
+            pin_dict[pin]['port'] = result.group(2)
+            pin_dict[pin]['bank'] = result.group(3)
+            pin_dict[pin]['type'] = result.group(4)
+            continue
+
+        # Other 1
+        result = re.match('#Other\s+net\s+PACKAGE_PIN\s+([A-Z]+[0-9]+)\s+-\s(\S+)\s+Bank\s+([0-9]+)\s+-\s+(\S+)', line)
+        if result:
+            pin = result.group(1)
+            pin_dict[pin]['name'] = result.group(2)
+            pin_dict[pin]['bank'] = result.group(3)
+            pin_dict[pin]['type'] = result.group(4)
+            continue
+
+        # Other 2
+        result = re.match('#Other\s+net\s+PACKAGE_PIN\s+([A-Z]+[0-9]+)\s+-\s+(No Connect)\s+Bank\s+([0-9]+)\s+-\s+(\S+)', line)
+        if result:
+            pin = result.group(1)
+            pin_dict[pin]['name'] = result.group(2)
+            pin_dict[pin]['bank'] = result.group(3)
+            pin_dict[pin]['type'] = result.group(4)
+            continue
+        print(line)
+
+    # pandas を使って CSV に変換
+    df = pd.DataFrame.from_dict(pin_dict, orient="index", columns=["port", "bank", "type", "name", "vcco"])
+    df.index.name = "pin"
+    df.to_csv(csv_fname)
+
+
+if __name__ == '__main__':
+    k26_som_xdc_to_csv("Kria_K26_SOM_Rev1.xdc", "k26-som-xck26-sfvc784-pin.csv")
